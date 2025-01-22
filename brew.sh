a98641d73d63cdabf8b475c02322a9f0d87453d7
Yumi Izumi <me@yumiizumi.com>
2025-01-22 21:27:17 +0900

    upload some dotfiles

---
+++ brew.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+regular_packages=(
+	"deno"
+	"node"
+	"docker"
+	"ollama"
+	"starship"
+	"atuin"
+	"gh"
+)
+
+cask_packages=(
+	"alt-tab"
+	"signal"
+	"ghostty"
+	"raycast"
+	"screen-studio"
+	"legcord"
+	"postman"
+	"nextdns"
+)
+
+echo "Installing regular packages..."
+for package in "${regular_packages[@]}"; do
+	echo "Installing $package..."
+	brew install "$package"
+done
+
+echo "Installing cask packages..."
+for package in "${cask_packages[@]}"; do
+	echo "Installing $package..."
+	brew install --cask "$package"
+done
+
+echo "All brew installations complete!"
