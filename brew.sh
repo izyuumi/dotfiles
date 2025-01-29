a5136f84c58e6dfa819beb878db828715c7f9932
Yumi Izumi <mail@yumi.to>
2025-01-29 17:57:13 +0900

    update

--- brew.sh
+++ brew.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
-if command -v brew >/dev/null 2>&1; then
-    echo "Homebrew is already installed"
+if command -v brew > /dev/null 2>&1; then
+  echo "Homebrew is already installed"
 else
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 regular_packages=(
-	"deno"
-	"node"
-	"docker"
-	"ollama"
-	"starship"
-	"atuin"
-	"gh"
-	"font-hack-nerd-font"
+  "deno"
+  "node"
+  "docker"
+  "ollama"
+  "starship"
+  "atuin"
+  "gh"
+  "font-hack-nerd-font"
   "prettierd"
-	"nextdns"
+  "nextdns"
   "ripgrep"
+  "shfmt"
 )
 
 cask_packages=(
-	"alt-tab"
-	"signal"
-	"ghostty"
-	"raycast"
-	"screen-studio"
-	"legcord"
-	"postman"
+  "alt-tab"
+  "signal"
+  "ghostty"
+  "raycast"
+  "screen-studio"
+  "legcord"
+  "postman"
 )
 
 echo "Installing regular packages..."
 for package in "${regular_packages[@]}"; do
-	echo "Installing $package..."
-	brew install "$package"
+  echo "Installing $package..."
+  brew install "$package"
 done
 
 echo "Installing cask packages..."
 for package in "${cask_packages[@]}"; do
-	echo "Installing $package..."
-	brew install --cask "$package"
+  echo "Installing $package..."
+  brew install --cask "$package"
 done
 
 echo "All brew installations complete!"
