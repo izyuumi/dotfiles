7571bd560313b553a3dc1a9670f031e21f08c443
Yumi Izumi <mail@yumi.to>
2025-01-29 17:57:13 +0900

    update

--- cargo.sh
+++ cargo.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if command -v cargo >/dev/null 2>&1; then
-    echo "Cargo is already installed"
+if command -v cargo > /dev/null 2>&1; then
+  echo "Cargo is already installed"
 else
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    source "$HOME/.cargo/env"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  source "$HOME/.cargo/env"
 fi
 
 cargo_packages=(
-	"eza"
-	"cargo-watch"
-	"cargo-generate"
+  "eza"
+  "cargo-watch"
+  "cargo-generate"
 )
 
 for package in "${cargo_packages[@]}"; do
-	echo "Installing $package..."
-	cargo install "$package"
+  echo "Installing $package..."
+  cargo install "$package"
 done
