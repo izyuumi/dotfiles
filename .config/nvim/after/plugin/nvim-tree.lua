8ca388ea3874b800cd48c8ee0b19f295b96ddfe4
Yumi Izumi <mail@yumi.to>
2025-01-29 16:46:35 +0900

    nvim-tree config

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -4,9 +4,18 @@ require("nvim-tree").setup({
 		float = {
 			enable = true
 		}
-	}
+	},
+  filters = {
+    custom = {
+      ".DS_Store"
+    }
+  }
 })
 
 vim.keymap.set("n", "<leader>o", "<CMD>NvimTreeToggle<CR>", { silent = true })
 vim.keymap.set("n", "<leader>e", "<CMD>NvimTreeFocus<CR>", { silent = true })
 
+local api = require("nvim-tree.api")
+api.events.subscribe(api.events.Event.FileCreated, function(file)
+  vim.cmd("edit " .. vim.fn.fnameescape(file.fname))
+end)
